f50ffa5fa18e361253a2d582298ae4b6f0675e9a
Yusuke Shibata <yusukeshib@gmail.com>
2025-03-22 20:16:08 -0400

    Preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,75 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { downloadBlob, browse } from "./utils";
-import { convertFoldToGltf } from "./converter";
+import { Converter, createConverter } from "./converter";
 
 function App() {
-  const [error, setError] = useState("");
-  const handleClick = async () => {
+  const [loaded, setLoaded] = useState<
+    | { type: "unloaded" }
+    | { type: "loaded"; converter: Converter }
+    | { type: "error"; error: string }
+  >({ type: "unloaded" });
+
+  const load = async () => {
     try {
-      setError("");
-      const file = await browse({});
-      const blob = await convertFoldToGltf(file);
-      downloadBlob("export.gltf", blob);
+      setLoaded({ type: "unloaded" });
+      const file = await browse({ accept: ".fold" });
+      const converter = await createConverter(file);
+      setLoaded({ type: "loaded", converter });
     } catch (err) {
-      setError(err instanceof Error ? err.message : `${err}`);
+      const error = err instanceof Error ? err.message : `${err}`;
+      setLoaded({ type: "error", error });
+    }
+  };
+
+  const download = async () => {
+    if (loaded.type === "loaded") {
+      const blob = await loaded.converter.exportGLTF();
+      downloadBlob("export.gltf", blob);
     }
   };
 
   return (
     <>
-      <p className="read-the-docs">
-        Open your FOLD file, it generates your GLTF file with animation.
+      <p>
+        <button onClick={load}>Load FOLD file</button>
       </p>
-      <button onClick={handleClick}>Browse</button>
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {loaded.type === "error" && (
+        <p style={{ color: "red" }}>{loaded.error}</p>
+      )}
+      {loaded.type === "loaded" && (
+        <>
+          <Preview converter={loaded.converter} />
+          <p>
+            <button onClick={download}>Download GLTF</button>
+          </p>
+        </>
+      )}
     </>
+  );
+}
+
+function Preview({ converter }: { converter: Converter }) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const elem = ref.current;
+    if (!elem) return;
+
+    elem.appendChild(converter.renderer.domElement);
+    return () => {
+      elem.removeChild(converter.renderer.domElement);
+    };
+  }, [converter]);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        background: "black",
+        width: converter.width,
+        height: converter.height,
+      }}
+    />
   );
 }
 
--- src/converter.ts
+++ src/converter.ts
@@ -2,26 +2,52 @@ import * as THREE from "three";
 import { GLTFExporter } from "three/examples/jsm/Addons.js";
 import { readText } from "./utils";
 
-export const convertFoldToGltf = async (file: File) => {
-  // Load FOLD file
+type Resolved<T> = T extends Promise<infer R> ? R : never;
+
+export type Converter = Resolved<ReturnType<typeof createConverter>>;
+
+export const createConverter = async (file: File) => {
   const jsonStr = await readText(file);
   const json = JSON.parse(jsonStr);
+  const width = 640;
+  const height = 480;
 
-  const scene = createScene(json);
+  // TODO:
 
-  const exporter = new GLTFExporter();
-
-  const result = await new Promise<ArrayBuffer>((resolve, reject) => {
-    exporter.parse(scene, (gltf) => resolve(gltf as ArrayBuffer), reject, {
-      binary: true,
-    });
-  });
-
-  return new Blob([result]);
-};
-
-function createScene(json: any) {
   const scene = new THREE.Scene();
-  // TODO: Setup scene
-  return scene;
-}
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  const exportGLTF = async () => {
+    const exporter = new GLTFExporter();
+
+    const result = await new Promise<ArrayBuffer>((resolve, reject) => {
+      exporter.parse(scene, (gltf) => resolve(gltf as ArrayBuffer), reject, {
+        binary: true,
+      });
+    });
+
+    return new Blob([result]);
+  };
+
+  const animate = () => {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  };
+  renderer.setAnimationLoop(animate);
+
+  return {
+    renderer,
+    exportGLTF,
+    width,
+    height,
+  };
+};
